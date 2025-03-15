e1e523968eb6daf637c4cbdf3dd2b646c54ffd47
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-03-14 22:44:47 -0500

    Metodos

--- src/test/java/models/Curso.java
+++ src/test/java/models/Curso.java
@@ -1,12 +1,43 @@
 package models;
 
 public class Curso {
-private String nombre;
-private int duracion;
-public Curso(String nombre, int duracion, String horario) {
-    this.nombre = nombre;
-    this.duracion = duracion;
-    this.horario = horario;
-}
-private String horario;
+    private String nombre;
+    private int duracion;
+    private String horario; // Declaración del atributo horario
+
+    // Constructor sin parámetros
+    public Curso() {
+    }
+
+    // Constructor con parámetros
+    public Curso(String nombre, int duracion, String horario) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.horario = horario;
+    }
+
+    // Métodos getters y setters 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
 }
