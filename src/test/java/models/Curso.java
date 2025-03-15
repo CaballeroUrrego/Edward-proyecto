b4782b54733fcfe2e47eaa7cb78e14c3a0f912ab
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-03-14 22:36:37 -0500

    Funcion sprig bot 2 parte 1

---
+++ src/test/java/models/Curso.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Curso {
+private String nombre;
+private int duracion;
+public Curso(String nombre, int duracion, String horario) {
+    this.nombre = nombre;
+    this.duracion = duracion;
+    this.horario = horario;
+}
+private String horario;
+}
