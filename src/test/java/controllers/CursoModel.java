63897a01f6b65aa20d1aa045311eb4979a3ea60a
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-03-14 22:54:28 -0500

    Agregar Carpeta Curso Model

--- src/test/java/controllers/CursoModel.java
+++ src/test/java/controllers/CursoModel.java
@@ -1,5 +1,16 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType; // Asegurarse de importar MediaType
+
+import models.Curso;
+
+@RestController
 public class CursoModel {
 
+    @GetMapping(value = "curso", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Curso getCurso() {
+        return new Curso("Java", 100, "mañana");
+    }
 }
